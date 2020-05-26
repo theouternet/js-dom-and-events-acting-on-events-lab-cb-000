de1be219ca7c7a5a3d08a88cc779b8debe72d382
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-26 23:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function retrieveEmployeeInformation() {
     return input.value
 }
 
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
